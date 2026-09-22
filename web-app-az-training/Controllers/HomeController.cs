596c8a8b3b27401ebbb3374538895b639ca23494
AAyush <aayush@manektech.com>
2026-09-22 23:34:19 +0530

    Add project files.

---
+++ web-app-az-training/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using web_app_az_training.Models;
+
+namespace web_app_az_training.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
