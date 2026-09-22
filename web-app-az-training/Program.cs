cca6bb14f15667c85139b4d681459863486dafcb
AAyush <aayush@manektech.com>
2026-09-22 23:59:17 +0530

    added telemetry logs

--- web-app-az-training/Program.cs
+++ web-app-az-training/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+//Add Telemetry services to the container.
+builder.Services.AddApplicationInsightsTelemetry();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
